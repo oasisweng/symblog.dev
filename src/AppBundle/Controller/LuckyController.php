bda32cc4449b06d1f781e23250f26c74705a38d5
oasisweng <oasisweng@gmail.com>
2016-01-21 23:04:01 +0000

    add luck

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+class LuckyController extends Controller {
+    /**
+     *
+     *
+     * @Route("/lucky/number/{count}")
+     */
+    public function numberAction($count) {
+        $numbers = array();
+        for ($i = 0; $i < $count; $i++) {
+            $numbers[] = rand(0, 100);
+        }
+        $numbersList = implode(', ', $numbers);
+
+        // $html = $this->container->get('templating')->render(
+        //     'lucky/number.html.twig',
+        //     array('luckyNumberList' => $numbersList)
+        // );
+
+        // return new Response($html);
+        
+        return $this->render(
+        'lucky/number.html.twig',
+        array('luckyNumberList' => $numbersList)
+        );
+    }
+
+    /**
+     *
+     *
+     * @Route("/api/lucky/number")
+     */
+    public function apiNumberAction() {
+        $data = array(
+            'lucky_number' => rand( 0, 100 ),
+        );
+
+        return new JsonResponse($data);
+    }
+}
+
+
